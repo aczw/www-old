72bce414b04177b757f6e67d14ba826cfd8f70be
Charles Wang <charleszw16@gmail.com>
2023-06-30 02:28:45 -0400

    hell

--- src/pages/tic-tac-toe/index.tsx
+++ src/pages/tic-tac-toe/index.tsx
@@ -73,50 +73,27 @@ function Board({
     onPlay(nextSquares);
   }
 
+  const board: React.ReactElement[] = [];
+
+  for (let i = 0; i < 3; i++) {
+    board.push(
+      <div>
+        {Array(3)
+          .fill(<></>)
+          .map((_, j) => (
+            <Square
+              key={j + i * 3}
+              value={squares[j + i * 3] ?? ""}
+              onSquareClick={() => handleClick(j + i * 3)}
+            />
+          ))}
+      </div>
+    );
+  }
+
   return (
     <>
-      <div>
-        <Square
-          value={squares[0] ?? ""}
-          onSquareClick={() => handleClick(0)}
-        />
-        <Square
-          value={squares[1] ?? ""}
-          onSquareClick={() => handleClick(1)}
-        />
-        <Square
-          value={squares[2] ?? ""}
-          onSquareClick={() => handleClick(2)}
-        />
-      </div>
-      <div>
-        <Square
-          value={squares[3] ?? ""}
-          onSquareClick={() => handleClick(3)}
-        />
-        <Square
-          value={squares[4] ?? ""}
-          onSquareClick={() => handleClick(4)}
-        />
-        <Square
-          value={squares[5] ?? ""}
-          onSquareClick={() => handleClick(5)}
-        />
-      </div>
-      <div>
-        <Square
-          value={squares[6] ?? ""}
-          onSquareClick={() => handleClick(6)}
-        />
-        <Square
-          value={squares[7] ?? ""}
-          onSquareClick={() => handleClick(7)}
-        />
-        <Square
-          value={squares[8] ?? ""}
-          onSquareClick={() => handleClick(8)}
-        />
-      </div>
+      <div>{board}</div>
     </>
   );
 }
