9ef96c3a535859c9749471fb0f70624a1b43ebf6
Charles Wang <charleszw16@gmail.com>
2023-07-08 02:11:02 -0400

    start homepage work

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,27 @@
 import Head from "next/head";
 import Link from "next/link";
 
-export default function Home() {
+const Home = () => {
   return (
     <>
       <Head>
-        <title>Create T3 App</title>
+        <title>aczw</title>
         <link
           rel="icon"
           href="/favicon.ico"
         />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            web dev{" "}
-            <span className="text-[hsl(280,100%,70%)]">
-              <i>yeah</i>
-            </span>
-          </h1>
-          <div>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="/ttt"
-            >
-              <h3 className="text-2xl font-bold">tic tac toe!</h3>
-              <div className="text-lg">I hate my life</div>
-            </Link>
-          </div>
-        </div>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-otherworld-600">
+        <div className="mb-10 text-5xl font-bold text-otherworld-100">aczw</div>
+        <Link
+          href="/ttt"
+          className="rounded-xl bg-otherworld-500 p-8 text-xl text-otherworld-100 hover:bg-otherworld-400"
+        >
+          tic tac toe
+        </Link>
       </main>
     </>
   );
-}
+};
+
+export default Home;
